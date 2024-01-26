333ef28f6fbfd44e9cdc0baf66a4dc7f2d628710
Itachiprox <157369200+Itachiprox@users.noreply.github.com>
2024-01-26 12:52:37 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -16,16 +16,16 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-OWNER_ID = 5228565677
-sudo_users = ["5228565677"]
-GROUP_ID = -1002037503706
-TOKEN = "6852378560:AAHl6-3MW_eiE72v2R5X-8EBYghKTon4kU0"
+OWNER_ID = 6590287973
+sudo_users = ["6590287973"]
+GROUP_ID = -1002084824749
+TOKEN = "6766361235:AAHggdaeIzbdKLeY53l_kX_QTeIpvK6T8JY"
 mongo_url = "mongodb+srv://itachi:itachi@itachi.hyhnjlq.mongodb.net/?retryWrites=true&w=majority"
 PHOTO_URL = ["https://telegra.ph/file/72ea883532b722f405059.jpg", "https://telegra.ph/file/72ea883532b722f405059.jpg"]
-SUPPORT_CHAT = "bot_support89"
-UPDATE_CHAT = "itachiprox"
+SUPPORT_CHAT = "narutobot12"
+UPDATE_CHAT = "catchlogs"
 BOT_USERNAME = "AnzooBot"
-CHARA_CHANNEL_ID = -1002037503706
+CHARA_CHANNEL_ID = -1002107229456
 api_id = 24658337
 api_hash = "bf99242dbb7f3501f28d39f0a0383cbf"
 
